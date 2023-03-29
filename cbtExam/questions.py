a079b86d19c9d104eea6a2c01fab20ab60b64c99
Jamiu Adewale Yusuf <jamiuadewaleyusuf@gmail.com>
2023-03-29 20:43:13 +0100

    creating the question class

---
+++ cbtExam/questions.py
@@ -0,0 +1,114 @@
+class Questions:
+
+    def question1():
+        q1 = f"1. Who developed Python Programming Language?\n"
+        option1 = {
+            'A': "Wick van Rossum",
+            'B': "Rasmus Lerdorf",
+            'C': "Guido van Rossum",
+            'D': "Niene Stom"
+        }
+        print(q1, option1)
+
+    def question2():
+      q2 = f"2. Which type of Programming does Python support?\n"
+      option2 = {
+            'A': "object-oriented programming",
+            'B': "structured programming",
+            'C': "functional programming",
+            'D': "All of the above"
+        }
+      print("\n\n")
+      print(q2, option2)
+
+    def question3():
+        q3 = f"3. Is Python case sensitive when dealing with identifiers?\n"
+        option3 = {
+            'A': "machine dependent",
+            'B': "yes",
+            'C': "no",
+            'D': "none of the above"
+        }
+        print("\n\n")
+        print(q3, option3)
+
+    def question4():
+     q4 = f"4. Which of the following is the correct extension of the Python file?\n"
+     option4 = {
+            'A': ".python",
+            'B': ".py",
+            'C': ".pl",
+            'D': ".p"
+        }
+     print("\n\n")
+     print(q4, option4)
+
+    def question5():
+     q5 = f"5. Is Python code compiled or interpreted\n"
+     option5 = {
+            'A': "Python code is both compiled and interpreted",
+            'B': "Python code is neither compiled nor interpreted",
+            'C': "Python code is only compiled",
+            'D': "Python code is only interpreted"
+        }
+     print("\n\n")
+     print(q5, option5)
+
+    
+    def question6():
+        q6 = f"6. All keywords in Python are in _________\n"
+        option6 = {
+            'A': "Capitalized",
+            'B': "Lowercase",
+            'C': "Upper",
+            'D': "None of the above"
+        }
+        print("\n\n")
+        print(q6, option6)
+
+    def question7():
+        q7 = f"7. What will be the value of the following Python expression?\n 4 + 3 % 5"
+        option7 = {
+            'A': "7",
+            'B': "2",
+            'C': "4",
+            'D': "1"
+        }
+    
+        print("\n\n")
+        print(q7, option7)
+
+    def question8():
+        q8 = f"8. Which of the following is used to define a block of code in Python language?\n"
+        option8 = {
+            'A': "Indentation",
+            'B': "Key",
+            'C': "Bracket",
+            'D': "All of the above"
+        }
+        print("\n\n")
+        print(q8, option8)
+
+    def question9():
+        q9 = f"9. Which keyword is used for function in Python language?\n"
+        option9 = {
+            'A': "Function",
+            'B': "def",
+            'C': "fun",
+            'D': "Define"
+        }
+    
+        print("\n\n")
+        print(q9, option9)
+
+    def question10():
+        q10 = f"10.  Which of the following character is used to give single-line comments in Python?\n"
+        option10 = {
+            'A': "//",
+            'B': "#",
+            'C': "/* ",
+            'D': "!"
+        }
+     
+        print("\n\n")
+        print(q10, option10)
